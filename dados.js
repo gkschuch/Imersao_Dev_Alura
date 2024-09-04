90632bb7a66b9f69e1840d95d7dcabd3fd62f269
Gabriel Schuch <gksgabriel2004@gmail.com>
2024-09-04 18:17:27 -0300

    Implemento da base de dados para o site

---
+++ dados.js
@@ -0,0 +1,50 @@
+let dados = [
+    {
+        titulo: 'Interstellar',
+        descricao: 'Interestellar é uma emocionante saga de ficção científica que nos leva em uma jornada épica através do cosmos. Em um futuro próximo, a Terra está à beira da colapso, e um grupo de astronautas embarca em uma missão desesperada para encontrar um novo lar para a humanidade. Liderados pelo piloto Cooper, eles exploram um buraco de minhoca recém-descoberto, que oferece a esperança de alcançar galáxias distantes e planetas potencialmente habitáveis. O filme explora temas como amor, sacrifício e a busca por significado, enquanto nos presenteia com visuais deslumbrantes e efeitos especiais inovadores.',
+        link: 'https://www.imdb.com/title/tt0816692/',
+        nota: '8.7'
+    },
+    {
+        titulo: 'Oppenheimer',
+        descricao: 'Oppenheimer é um filme biográfico que retrata a vida de J. Robert Oppenheimer, o físico teórico americano que é considerado o "pai da bomba atômica". O longa-metragem explora a criação da primeira arma nuclear e as consequências éticas dessa descoberta, além de apresentar um retrato íntimo do cientista e seu dilema moral.',
+        link: 'https://www.imdb.com/title/tt15398776/?ref_=nv_sr_srsg_0_tt_5_nm_3_in_0_q_opp',
+        nota: '8.3'
+    },
+    {
+        titulo: 'Breaking Bad',
+        descricao: 'Breaking Bad é uma série de televisão americana que acompanha a transformação de Walter White, um professor de química do ensino médio diagnosticado com câncer, em um poderoso chefão do tráfico de drogas. A série explora temas como moralidade, família, poder e as consequências das escolhas.',
+        link: 'https://www.imdb.com/title/tt0903747/?ref_=nv_sr_srsg_3_tt_8_nm_0_in_0_q_break',
+        nota: '9.5'
+    },
+    {
+        titulo: 'Game of Thrones',
+        descricao: 'Game of Thrones é uma épica série de fantasia que nos transporta para o mundo de Westeros, um continente medieval fictício onde grandes famílias nobres travam uma guerra implacável pelo controle do Trono de Ferro. Com personagens complexos e moralmente ambíguos, a série explora temas como poder, traição, amor e morte. A trama, baseada na saga literária "As Crônicas de Gelo e Fogo" de George R.R. Martin, é marcada por reviravoltas inesperadas e batalhas épicas.',
+        link: 'https://www.imdb.com/title/tt0944947/',
+        nota: '9.2'
+    },
+    {
+        titulo: 'O Poderoso Chefão',
+        descricao: 'O Poderoso Chefão é um clássico do cinema que imortalizou a saga da família Corleone, um dos clãs mafiosos mais poderosos dos Estados Unidos. Dirigido por Francis Ford Coppola, o filme nos transporta para o mundo do crime organizado, onde lealdade, traição e poder absoluto são a ordem do dia. Através da história de Vito Corleone e seus filhos, somos apresentados a um universo complexo e fascinante, repleto de personagens carismáticos e diálogos memoráveis.',
+        link: 'https://www.imdb.com/title/tt0068646/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_godfather',
+        nota: '9.2'
+    },
+    {
+        titulo: 'O Senhor dos Anéis: A Sociedade do Anel',
+        descricao: 'O Senhor dos Anéis: A Sociedade do Anel é um épico cinematográfico que transportou milhões de espectadores para a Terra-Média, um mundo de fantasia criado por J.R.R. Tolkien. Acompanhamos a jornada de Frodo Bolseiro, um hobbit que descobre um anel mágico capaz de dominar o mundo, e a Sociedade do Anel, formada por diversos personagens, que se une para destruí-lo. Com visuais deslumbrantes, uma trilha sonora memorável e uma narrativa envolvente, o filme se tornou um marco na história do cinema, cativando fãs de todas as idades e inspirando diversas outras obras. A Sociedade do Anel é mais do que um filme de aventura; é uma jornada épica sobre amizade, coragem e a luta entre o bem e o mal.',
+        link: 'https://www.imdb.com/title/tt0120737/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_fellowship',
+        nota: '8.9'
+    },
+    {
+        titulo: 'Coringa',
+        descricao: '"Coringa" é um filme psicológico que mergulha na mente torturada de Arthur Fleck, um comediante fracassado em Gotham City. A trama explora a transformação de Arthur em um dos vilões mais icônicos dos quadrinhos, o Coringa. Com uma atuação magistral de Joaquin Phoenix, o filme apresenta uma visão sombria e realista da origem do personagem, explorando temas como alienação social, saúde mental e a violência urbana. A direção de Todd Phillips cria uma atmosfera opressiva e claustrofóbica, intensificando a jornada de Arthur rumo à loucura.',
+        link: 'https://www.imdb.com/title/tt7286456/?ref_=nv_sr_srsg_2_tt_6_nm_0_in_0_q_joker',
+        nota: '8.4'
+    },
+    {
+        titulo: 'Parasita',
+        descricao: '"Parasita" é um filme sul-coreano que rapidamente se tornou um fenômeno mundial, conquistando o Oscar de Melhor Filme em 2020. A trama, com uma mistura de suspense e comédia negra, gira em torno de duas famílias de classes sociais opostas que se entrelaçam de forma inesperada. Através de uma narrativa envolvente e personagens complexos, o diretor Bong Joon-ho apresenta uma crítica social contundente sobre a desigualdade, a pobreza e a luta pela sobrevivência.',
+        link: 'https://www.imdb.com/title/tt6751668/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_parasi',
+        nota: '8.6'
+    }
+];
