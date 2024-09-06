f302c532440cb4044985fe10980b010b3f7a56aa
Gabriel Schuch <gksgabriel2004@gmail.com>
2024-09-06 17:11:52 -0300

    Implementacao da funcao pesquisar

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 function pesquisar() {
-    // Obtém a seção onde os resultados serão exibidos
-    let section = document.getElementById('resultados-pesquisa');
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById('resultados-pesquisa');
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = '';
+  let campoPesquisa = document.getElementById('campo-pesquisa').value;
 
-    // Itera sobre os dados e constrói o HTML para cada resultado
-    for (let dado of dados) {
-        // Cria um novo elemento div para cada resultado
-        resultados += `
-        <div class="item-resultado">
-          <h2>${dado.titulo}</h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <h3><a href="${dado.link}"target="_blank">IMDb</a></h3>
-          <h4 class="descricao-meta">Nota: ${dado.nota}</h4>
-        </div>
-      `;
+  if (campoPesquisa == '') {
+    section.innerHTML = '<p>Nada foi encontrado. Digite o nome do filme ou série</p>'
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultados = '';
+  let titulo = '';
+
+  // Itera sobre os dados e constrói o HTML para cada resultado
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    if (titulo.includes(campoPesquisa)) {
+      // Cria um novo elemento div para cada resultado
+      resultados += `
+      <div class="item-resultado">
+        <h2>${dado.titulo}</h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <h3><a href="${dado.link}"target="_blank">IMDb</a></h3>
+        <h4 class="descricao-meta">Nota: ${dado.nota}</h4>
+      </div>
+    `;
     }
-    // Atribui o HTML gerado à seção de resultados
-    section.innerHTML = resultados;
+  }
+
+  if (!resultados) {
+    resultados = '<p>Nada foi encontrado. O filme ou série não está no meu banco de dados</p>'
+  }
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultados;
 }
